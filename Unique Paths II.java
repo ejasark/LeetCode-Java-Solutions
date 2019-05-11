f7bf7f09066cba2e4bf212208684bd02e4514759
ejasark <sarkar.jayati.1989@gmail.com>
2019-05-11 18:14:00 +0530

    Update Unique Paths II.java

--- Unique Paths II.java
+++ Unique Paths II.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+    public int uniquePathsWithObstacles(int[][] mat) {
+        int[][] dp = new int[mat.length][mat[0].length];
 
         for (int i=0; i<dp.length; i++) {
-            if (obstacleGrid[i][0] == 1) {
+            if (mat[i][0] == 1) {
                 break;
             }
             dp[i][0] = 1;
         }
 
         for (int i=0; i<dp[0].length; i++) {
-            if (obstacleGrid[0][i] == 1) {
+            if (mat[0][i] == 1) {
                 break;
             }
             dp[0][i] = 1;
@@ -18,12 +18,12 @@ class Solution {
 
         for (int i=1; i<dp.length; i++) {
             for (int j=1; j<dp[0].length; j++) {
-                if (obstacleGrid[i][j] != 1) {
+                if (mat[i][j] != 1) {
                     dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
         }
         
-        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+        return dp[mat.length-1][mat[0].length-1];
     }
 }
