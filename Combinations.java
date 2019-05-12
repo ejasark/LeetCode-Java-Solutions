dfe719245cf9bbc70921ae762a873f89ef7bf928
ejasark <sarkar.jayati.1989@gmail.com>
2019-05-13 01:03:51 +0530

    Update Combinations.java

--- Combinations.java
+++ Combinations.java
@@ -1,26 +1,19 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        helper(n, 1, temp, ans, k);
-        
-        return ans;
+        combinations(n, 0, temp, res, k);
+        return res;
     }
     
-    private void helper(int n, int start, List<Integer> temp, List<List<Integer>> ans, int len) {
-        if (temp.size() == len) {
-            ans.add(new ArrayList<>(temp));
+    private void combinations(int n, int start, List<Integer> temp, List<List<Integer>> res, int k) {
+        if (temp.size() == k) {
+            res.add(new ArrayList<>(temp));
             return;
         }
-        
         for (int i=start; i<=n; i++) {
-            // Choose
             temp.add(i);
-            
-            // Explore
-            helper(n, i+1, temp, ans, len);
-            
-            // Un-choose
+            combinations(n, i+1, temp, ans, len);
             temp.remove(temp.size() - 1);
         }
     }
