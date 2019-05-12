0b7ea1112ca97a459001f307ca9c42aeadc28d02
ejasark <sarkar.jayati.1989@gmail.com>
2019-05-13 01:10:48 +0530

    Update Subsets.java

--- Subsets.java
+++ Subsets.java
@@ -1,20 +1,18 @@
 class Solution {
-    public static List<List<Integer>> subsets(int[] nums) {
-
-        Arrays.sort(nums);
-        List<List<Integer>> perms = new ArrayList<>();
+    public static List<List<Integer>> subsets(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        subsetsHelper(nums, perms, temp, 0);
-
-        return perms;
+        computeSubsets(arr, res, temp, 0);
+        return res;
     }
 
-    private static void subsetsHelper(int[] nums, List<List<Integer>> perms, List<Integer> temp, int start) {
-        perms.add(new ArrayList<>(temp));
+    private static void computeSubsets(int[] nums, List<List<Integer>> res, List<Integer> temp, int start) {
+        res.add(new ArrayList<>(temp));
 
-        for (int i=start; i<nums.length; i++) {
-            temp.add(nums[i]);
-            subsetsHelper(nums, perms, temp, i+1);
+        for (int i=start; i<arr.length; i++) {
+            temp.add(arr[i]);
+            computeSubsets(arr, res, temp, i+1);
             temp.remove(temp.size()-1);
         }
     }
